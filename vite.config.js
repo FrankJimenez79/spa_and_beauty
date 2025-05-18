fdf39c6d89a3db1b8be272e2fa8c7ef835ed7670
Frank Jimenez <frankjb79@gmail.com>
2025-05-18 16:09:36 +0200

    Subiendo cambios para github pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/spa_and_beauty/",
   server: {
     watch: {
-      usePolling: true
-    }
-  }
-})
+      usePolling: true,
+    },
+  },
+});
